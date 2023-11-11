b906f8244d1d2a5977ee1414388de11f8cc50327
baudouinth <baudouin.theobald@gmail.com>
2023-11-11 15:15:43 +0100

    breakable block

--- .local/share/pandoc/filters/boxed-codeblock.lua
+++ .local/share/pandoc/filters/boxed-codeblock.lua
@@ -1,15 +1,13 @@
-function raw_tex (t)
+function raw_tex(t)
     return pandoc.RawBlock('tex', t)
-  end
-  
-  --- Wrap code blocks in tcolorbox environments
-  function CodeBlock (cb)
-    return {raw_tex'\\begin{tcolorbox}', cb, raw_tex '\\end{tcolorbox}'}
-  end
-  
-  --- Ensure that the longfbox package is loaded.
-  function Meta (m)
-    m['header-includes'] = {raw_tex '\\usepackage{tcolorbox}'}
+end
+
+function CodeBlock(cb)
+    return {raw_tex '\\begin{tcolorbox}[breakable]', cb, raw_tex '\\end{tcolorbox}'}
+end
+
+function Meta(m)
+    m['header-includes'] = {raw_tex [[\usepackage[most]{tcolorbox}
+        \tcbset{enhanced jigsaw}]]}
     return m
-  end
-  
+end
